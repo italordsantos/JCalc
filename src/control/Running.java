cc386d4546cd3cddd336c0b535c860efc457bb32
Italo Rodrigues <italo.rgrego@gmail.com>
2020-07-21 09:41:36 -0300

    Criação da estrutura básica

---
+++ src/control/Running.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import view.JCalc;
+
+/**
+ *
+ * @author italo
+ */
+public class Running {
+    
+    public static void main(String[] args) {
+        new JCalc().setVisible(true);
+    }
+}
